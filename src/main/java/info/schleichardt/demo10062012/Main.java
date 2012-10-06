cc407a34fc38998ec57e81547005bbd2b5879688
Michael Schleichardt <michael@schleichardt.net>
2012-10-06 14:55:23 +0200

    finished demo

--- src/main/java/info/schleichardt/demo10062012/Main.java
+++ src/main/java/info/schleichardt/demo10062012/Main.java
@@ -1,7 +1,29 @@
 package info.schleichardt.demo10062012;
 
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+
+/**
+ * Watches a folder in the system specific temp directory. You may have to create the folder.
+ *
+ * Example console output:
+ * <pre>{@code
+ *ENTRY_CREATE: /tmp/watchme/xyz.txt
+ *ENTRY_MODIFY: /tmp/watchme/xyz.txt
+ *ENTRY_DELETE: /tmp/watchme/xyz.txt
+ * }</pre>
+ *
+ * Use "gradle run" in the project root to start the app.
+ * Use "gradle javadoc" to create the javadocs.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("it runs");
+    public static void main(String[] args) throws IOException {
+        final String pathToWatchedFolder = System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") + "watchme";
+        try {
+            System.out.println("Watching folder " + pathToWatchedFolder + ".");
+            WatchDir.main(new String[]{"-r", pathToWatchedFolder});
+        } catch (final NoSuchFileException e) {
+            System.err.println(pathToWatchedFolder + " does not exist. Create it before watching.");
+        }
     }
 }
